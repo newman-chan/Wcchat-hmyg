49bc34e19d1e6581c9930c3da702cad03c5c7393
newman-chan <546171755@qq.com>
2019-07-30 14:56:50 +0800

    首页轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,29 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+//页面初始数据
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 轮播图数组
+    swiperList:[],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  //页面加载
+  onLoad(){
+    this.getSwiperList()
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+
+  //获取轮播图数据
+  getSwiperList(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+        // console.log(result);
+        // console.log(result.data.message);
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          swiperList:result.data.message
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+        
+      },
+    });
+      
   }
-})
+});
+  
