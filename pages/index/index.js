19bb134159c73007c4ce6173235cb202ba2c3a8b
newman-chan <546171755@qq.com>
2019-07-30 17:14:47 +0800

    首页楼层完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -50,7 +50,9 @@ Page({
       url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
       success: (result) => {
         console.log(result);
-        
+        this.setData({
+          floorList:result.data.message
+        })
       },
     });
   }
