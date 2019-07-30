ebfe568b5c0ce9e1ed373dc303c9e9547013400b
newman-chan <546171755@qq.com>
2019-07-30 16:07:18 +0800

    首页分类导航完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,17 @@ Page({
   data: {
     // 轮播图数组
     swiperList:[],
+    //分类导航数组
+    navList:[],
+    // 楼层数据
+    floorList:[]
   },
 
   //页面加载
   onLoad(){
     this.getSwiperList()
+    this.getNavList()
+    this.getFloorList()
   },
 
   //获取轮播图数据
@@ -22,8 +28,31 @@ Page({
         })
         
       },
+    });    
+  },
+
+  //获取分类导航数据
+  getNavList(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+        // console.log(result);
+        this.setData({
+          navList:result.data.message
+        })
+      },
     });
-      
+  },
+
+  //获取楼层数据
+  getFloorList(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success: (result) => {
+        console.log(result);
+        
+      },
+    });
   }
 });
   
